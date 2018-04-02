7efd9f83541f0f39d40298971c3ed97b5d65aac0
bojotamara <tamarabojovic@hotmail.com>
2018-04-02 16:56:21 -0600

    knight gen working

--- chess_classes.py
+++ chess_classes.py
@@ -1,8 +1,22 @@
-def check_bounds(x,y):
-    if x >= 0 and x < 8 and y >= 0 and y < 8:
+
+# a basic move check, that only checks if the space is occupied, or if
+# its a friendly piece
+def move_check(your_color, y, x ,board):
+
+    if x < 0 or x > 7 or y < 0 or y > 7:
+        return False
+
+    piece = board.array[y][x]
+
+    if piece == None:
         return True
     else:
-        return False
+
+        if piece.color != your_color:
+            return True
+        else:
+            return False
+
 
 
 class Board:
@@ -24,6 +38,7 @@ class Piece:
         self.color = color
         self.x = x
         self.y = y
+        self.move_list = []
 
     def gen_legal_moves():
         # returns a move-list thats a tuple
@@ -36,50 +51,42 @@ class Pawn(Piece):
 
     # returns a list that contains tuples where the piece can move
     def gen_legal_moves(self, board):
-        move_list = []
+
         offsets = [-1, 1]
 
         if self.color == "w":
             # normal move forward
             if board.array[self.y - 1][self.x] == None and check_bounds(self.y - 1,self.x):
-                move_list.append( (self.y - 1, self.x) )
+                self.move_list.append( (self.y - 1, self.x) )
 
             for diff in offsets:
                 newX = self.x + diff
                 newY = self.y - 1
-                if not check_bounds(newX,newY):
+
+                if not move_check(self.color,newY,newX,board):
                     continue
 
-                p = board.array[newY][newX]
-
-                if p == None:
-                    continue
-
-                if p.color == "b":
-                    move_list.append( (newY,newX))
+                else:
+                    self.move_list.append( (newY,newX))
 
 
 
         elif self.color == "b":
             # normal move forward
             if board.array[self.y + 1][self.x] == None and check_bounds(self.y + 1,self.x):
-                move_list.append( (self.y + 1, self.x) )
+                self.move_list.append( (self.y + 1, self.x) )
 
             for diff in offsets:
                 newX = self.x + diff
                 newY = self.y + 1
-                if not check_bounds(newX,newY):
+
+                if not move_check(self.color,newY,newX,board):
                     continue
 
-                p = board.array[newY][newX]
+                else:
+                    self.move_list.append( (newY,newX))
 
-                if p == None:
-                    continue
-
-                if p.color == "w":
-                    move_list.append( (newY,newx))
-
-        return move_list
+        return self.move_list
 
 class Rook(Piece):
 
@@ -96,6 +103,19 @@ class Knight(Piece):
     def __init__(self, color, y, x):
         super().__init__(color,y,x)
 
+    def gen_legal_moves(self, board):
+        offsets = [(-1,-2),(-1,2),(-2,-1),(-2,1),(1,-2),(1,2),(2,-1),(2,1)]
+
+        for offset in offsets:
+            newX = self.x + offset[0]
+            newY = self.y + offset[1]
+
+            if move_check(self.color,newY,newX,board):
+                self.move_list.append((newY,newX))
+
+        return self.move_list
+
+
 class King(Piece):
 
     def __init__(self, color, y, x):
@@ -109,9 +129,10 @@ class Queen(Piece):
 
 board = Board()
 
-board.array[6][6] = Pawn("w",6,6)
-board.array[5][5] = Pawn("w",5,5)
-board.array[5][7] = Pawn("b",5,7)
+board.array[6][6] = Knight("w",6,6)
+#board.array[1][2] = Knight("b",1,2)
+#board.array[5][5] = Pawn("w",5,5)
+#board.array[5][7] = Pawn("b",5,7)
 piece = board.array[6][6]
 list1 = piece.gen_legal_moves(board)
 print(list1)
