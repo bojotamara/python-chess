ae6e477b0f5e876d6e26142e24237749de9c08c6
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-04-07 13:07:49 -0600

    moved things around to make it clear what the main file is

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
 #!/usr/bin/python3
 import pygame
+
 # from assets import *
-from board import *
+from modules.board import *
 
-pygame.init()
+# pygame.init()
 
-screen = pygame.display.set_mode((800,60*8))
+screen = pygame.display.set_mode((800, 60 * 8))
 pygame.display.set_caption('Boss Ass Chess Game')
 
-#load background image
+# load background image
 bg = pygame.image.load("assets/chessboard.png")
-#blit like puts the image on there
+# blit like puts the image on there
 screen.blit(bg, [0, 0])
 
 # wk = pygame.image.load("assets/wking.png")
 # screen.blit(wk, [0,0])
 
-#board matrix
-board=Board()
+# board matrix
+board = Board()
+
+
 def update_board():
     screen.blit(bg, [0, 0])
     for row in board.array:
         for piece in row:
-            if piece: #if piece is not none
+            if piece:  # if piece is not none
                 print(piece)
-                s=pygame.image.load(piece.sprite)
-                pos = (piece.x*60,piece.y*60)
-                screen.blit(s,pos)
+                s = pygame.image.load(piece.sprite)
+                pos = (piece.x * 60, piece.y * 60)
+                screen.blit(s, pos)
+
 
 update_board()
 
-c=0
+c = 0
 clock = pygame.time.Clock()
 crashed = False
 
@@ -40,9 +44,9 @@ while not crashed:
             crashed = True
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                c+=60
-                screen.blit(bg, [0,0])
-                screen.blit(wk, [c,c])
+                c += 60
+                screen.blit(bg, [0, 0])
+                screen.blit(wk, [c, c])
         # update_board()
         print(event)
 
--- modules/board.py
+++ modules/board.py
@@ -1,22 +1,27 @@
-from pieces import *
+from modules.pieces import *
+
 
 class Board:
     #None is empty
     def __init__(self):
         self.empty = [[None for x in range(8)] for y in range(8)]
-        self.array =[
-        [Rook("b",0,0),Knight("b",0,1),Bishop("b",0,2),Queen("b",0,3),King("b",0,4),Bishop("b",0,5),Knight("b",0,6),Rook("b",0,7)],
-        [Pawn("b",1,i) for i in range(8)],
-        [None for x in range(8)],
-        [None for x in range(8)],
-        [None for x in range(8)],
-        [None for x in range(8)],
-        [Pawn("w",6,i) for i in range(8)],
-        [Rook("w",7,0),Knight("w",7,1),Bishop("w",7,2),Queen("w",7,3),King("w",7,4),Bishop("w",7,5),Knight("w",7,6),Rook("w",7,7)]
+        self.array = [
+            [Rook("b", 0, 0), Knight("b", 0, 1), Bishop("b", 0, 2), Queen("b", 0, 3), King(
+                "b", 0, 4), Bishop("b", 0, 5), Knight("b", 0, 6), Rook("b", 0, 7)],
+            [Pawn("b", 1, i) for i in range(8)],
+            [None for x in range(8)],
+            [None for x in range(8)],
+            [None for x in range(8)],
+            [None for x in range(8)],
+            [Pawn("w", 6, i) for i in range(8)],
+            [Rook("w", 7, 0), Knight("w", 7, 1), Bishop("w", 7, 2), Queen("w", 7, 3), King(
+                "w", 7, 4), Bishop("w", 7, 5), Knight("w", 7, 6), Rook("w", 7, 7)]
         ]
         # Maps (y,x) of a piece to a set containing all the legal moves
         self.score = 0
-        self.pvalue_dict = {King: 200, Queen: 9, Rook: 5, Knight: 3, Bishop: 3, Pawn: 1} # maps piecetype to relative value
+        # maps piecetype to relative value
+        self.pvalue_dict = {King: 200, Queen: 9,
+                            Rook: 5, Knight: 3, Bishop: 3, Pawn: 1}
 
     def move_piece(self, piece, y, x):
         oldx = piece.x
@@ -28,15 +33,15 @@ class Board:
 
     # this is not a good place for this function, testing stuff out
     # Determine if there's a check on the king of the color inputted
-    def determine_check(self,color,kingy,kingx):
+    def determine_check(self, color, kingy, kingx):
         flag = False
         if color == "w":
             for __, attacked in self.blackmoves.items():
-                if (kingy,kingx) in attacked:
+                if (kingy, kingx) in attacked:
                     flag = True
         elif color == "b":
             for __, attacked in self.whitemoves.items():
-                if (kingy,kingx) in attacked:
+                if (kingy, kingx) in attacked:
                     flag = True
         return flag
 
