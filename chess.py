5087921a7bc48149cc0d648c4b90698a2bf45823
Arunscape <8227297+Arunscape@users.noreply.github.com>
2018-04-08 15:10:54 -0600

    sidemenu partial

--- chess.py
+++ chess.py
@@ -1,9 +1,10 @@
-#------------- INITIALIZATIONS-------------------
+# ------------- INITIALIZATIONS-------------------
 import pygame
 
 # from assets import **
 
 pygame.init()
+pygame.font.init()  # for text
 
 screen = pygame.display.set_mode((800, 60 * 8))
 pygame.display.set_caption('Boss Ass Chess Game')
@@ -11,9 +12,18 @@ pygame.display.set_caption('Boss Ass Chess Game')
 from modules.board import *
 from modules.computer import *
 
-# load background image
-bg = pygame.image.load("assets/chessboard.png").convert()
+
 # blit like puts the image on there
+
+bg = pygame.image.load("assets/chessboard.png").convert()
+sidebg = pygame.image.load("assets/woodsidemenu.jpg").convert()
+player = 1  # 'AI' otherwise
+myfont = pygame.font.SysFont(None, 30)
+clippy = pygame.image.load("assets/cpu.jpg").convert()
+clippy = pygame.transform.scale(clippy, (160, 120))
+playeravatar = pygame.image.load("assets/avatar.jpg").convert()
+playeravatar = pygame.transform.scale(playeravatar, (160, 120))
+
 
 # board matrix
 board = Board()
@@ -22,14 +32,13 @@ all_sprites_list = pygame.sprite.Group()
 sprites = [piece for row in board.array for piece in row if piece]
 all_sprites_list.add(sprites)
 
-screen.blit(bg, (0, 0))
 all_sprites_list.draw(screen)
 # all_sprites_list = pygame.sprite.LayeredDirty(
 #     piece for row in b.array for piece in row if piece)
 
 clock = pygame.time.Clock()
 
-#----------- FUNCTIONS---------------------------------
+# ----------- FUNCTIONS---------------------------------
 
 
 def select_piece(color):
@@ -52,9 +61,16 @@ def select_square():
     y = y // 60
     return (y, x)
 
+
 def run_game():
+    # clippy avatar for computer player
+    global player, playeravatar, clippy
+
+    update_sidemenu()
+
+    # screen.blit(playeravatar, (550, 20))
+
     gameover = False
-    player = 1  # 'AI' otherwise
 
     selected = False  # indicates whether a piece is selected yet
     trans_table = dict()
@@ -90,6 +106,8 @@ def run_game():
                             # MOVE NOT IN CHECK WE GOOD
                             selected = False
                             player = "AI"
+                            # update avatar
+                            update_sidemenu()
                             # delete sprite
                             if dest:
                                 all_sprites_list.remove(dest)
@@ -117,12 +135,57 @@ def run_game():
             """
             # just go back to player one for now lol
             player = 1
-
+            pygame.time.wait(1000)
+            # switch to player player avatar
+            # screen.blit(playeravatar, (550, 20))
+            update_sidemenu()
         screen.blit(bg, (0, 0))
         all_sprites_list.draw(screen)
         pygame.display.update()
         clock.tick(60)
 
-if __name__ == "__main__":
 
+def update_sidemenu():
+    screen.blit(sidebg, (480, 0))
+    global playeravatar, clippy
+    if player == 1:
+        screen.blit(playeravatar, (550, 20))
+        textsurface = myfont.render('Your Turn!', False, (255, 255, 255))
+    elif player == 'AI':
+        screen.blit(clippy, (550, 20))
+        textsurface = myfont.render('CPU Thinking...', False, (255, 255, 255))
+    screen.blit(textsurface, (550, 150))
+
+
+def camstream():
+    # bulk of the camera code was no written by us, since it's just here for fun
+    # and does not contribute to the actual game in any meaningful way
+    # modified from https://gist.github.com/snim2/255151
+    DEVICE = '/dev/video0'
+    SIZE = (640, 480)
+    FILENAME = 'assets/avatar.jpg'
+    import pygame.camera
+    pygame.camera.init()
+    display = pygame.display.set_mode((800, 60 * 8), 0)
+    camera = pygame.camera.Camera(DEVICE, SIZE)
+    camera.start()
+    screen = pygame.surface.Surface(SIZE, 0, display)
+    screen = camera.get_image(screen)
+    # display.blit(screen, (0, 0))
+    # pygame.display.flip()
+    # for event in pygame.event.get():
+    #     if event.type == pygame.QUIT:
+    #         capture = False
+    #     elif event.type == pygame.KEYDOWN and event.key == K_s:
+    pygame.image.save(screen, FILENAME)
+    camera.stop()
+    return
+
+
+if __name__ == "__main__":
+    camstream()
     run_game()
+
+    # delete picture taken
+    # from os import remove
+    # remove('assets/avatar.jpg')
